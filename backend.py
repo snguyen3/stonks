2d46f213640effc538fd8986aee57d8ed989d793
John Dill <jdill2@jh.edu>
2021-01-30 18:23:08 -0500

    updated backend.py

--- backend.py
+++ backend.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template, request, redirect
+import random
+import time
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=".", static_folder=".")
 
 @app.route('/', methods=['GET'])
-@app.route('/dashboard', methods=['GET'])
+@app.route('/index.html', methods=['GET'])
 def dashboard():
-    data = []
+    data = {}
+    for i,t in enumerate(['NASDAQ','GME','BTC','DOGE']):
+        data[t] = {}
+        data[t]['prices'] = [(time.time()-random.randint(0,10), random.uniform(1,100)) for i in range(50)]
+        data[t]['sentiments'] = [(time.time()-random.randint(0,10), random.uniform(-1, 1)) for i in range(50)]
+
     return render_template('index.html', data=data)
 
 if __name__ == '__main__':
