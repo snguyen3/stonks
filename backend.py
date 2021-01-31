f1d4db382b05e571e5965dc9cb2a473e6bc478be
John Dill <jdill2@jh.edu>
2021-01-30 22:46:15 -0500

    Starting to integrate WSBSentimentAnalyzer into the backend

--- backend.py
+++ backend.py
@@ -2,15 +2,26 @@ from flask import Flask, render_template, request, redirect
 import random
 import time
 import main
+import WSBSentimentAnalyzer
 
 app = Flask(__name__)
+
+analyzer = WSBSentimentAnalyzer.SentimentAnalyzer()
+parser = WSBSentimentAnalyzer.RedditDataParser()
+#data = parser.getRedditData('wsb-sample.csv', 80000)
+#parsed_data = parser.parseData(data)
 
 @app.route('/', methods=['GET'])
 @app.route('/index.html', methods=['GET'])
 def dashboard():
+
+
+
     data = {}
     tickers = ['NDX','GME','AAPL','SPY']
     stock_data = main.getStockData(tickers, "1mo", "1d")
+
+
     for i,d in enumerate(stock_data):
         data[d['name']] = {}
         data[d['name']]['prices'] = d['data']
